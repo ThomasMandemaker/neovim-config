e470646aba017fc9d6455de32308b51a8702f446
Thomas Mandemaker Productivity <thomasmandemakerproductivity@Thomass-MBP.lan>
2023-01-30 11:45:01 +0100

    Updated mapping

--- lua/deez/remap.lua
+++ lua/deez/remap.lua
@@ -1,2 +1,36 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv = gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv = gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+
+local function check()
+    local char = vim.api.nvim_get_current_line()
+    local bruh = string.sub(char, 0, 2)
+    print(bruh)
+    if string.find(bruh, '/') == nill then
+        return "^<C-v>I//<Esc>"
+    else
+        return "^<C-v>ld<Esc>"
+    end
+end
+
+vim.keymap.set("v", "<C-/>", function() return check() end, {expr = true, noremap = false})
+vim.keymap.set("n", "<C-/>", function() return check() end, {expr = true, noremap = false})
+
+vim.keymap.set("n", "<leader>c", ":!cargo run<CR>")
+vim.keymap.set("n", "<leader>wc", ":w !cargo run<CR>")
