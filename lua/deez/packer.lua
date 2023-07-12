b9b84efe3895ade4c55c7f5f84ca2855e4f4737c
thomasmandemaker <thomas.mandemaker@ihomer.nl>
2023-07-12 10:12:34 +0200

    Added: Java LSP functionality. Beware of hardcoded java path!

--- lua/deez/packer.lua
+++ lua/deez/packer.lua
@@ -35,6 +35,7 @@ return require('packer').startup(function(use)
 		  {'neovim/nvim-lspconfig'},             -- Required
 		  {'williamboman/mason.nvim'},           -- Optional
 		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+          {'mfussenegger/nvim-jdtls'},
 
 		  -- Autocompletion
 		  {'hrsh7th/nvim-cmp'},         -- Required
